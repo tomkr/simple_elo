bc69b38f8b4acaa1a09daedfe031daac51b1e77b
Tom Kruijsen <tom@tomkr.org>
2015-04-21 08:45:48 +0200

    Add calculating of new rating for a player.

--- lib/simple_elo.rb
+++ lib/simple_elo.rb
@@ -3,6 +3,11 @@ require "simple_elo/version"
 module SimpleElo
   extend self
 
+  def calculate(player_rating, opponent_rating, k_factor, result)
+    expected = expected(player_rating, opponent_rating)
+    player_rating + modifier(result, expected, k_factor)
+  end
+
   def expected(rating_a, rating_b)
     1.0 / ( 1.0 + ( 10.0 ** ((rating_a.to_f - rating_b.to_f) / 400.0) ) )
   end
--- spec/lib/simple_elo_spec.rb
+++ spec/lib/simple_elo_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe SimpleElo do
-  it "calculates the expected value" do
+  it "calculates a player's rating" do
+    player_rating = 1500.0
+    opponent_rating = 1500.0
+    k_factor = 20
+    expect(SimpleElo.calculate(player_rating, opponent_rating, k_factor, 1)).to eq(1510)
+  end
+
+  it "calculates the expected result" do
     expect(SimpleElo.expected(1500.0, 1500.0)).to eq(0.5)
   end
 
-  it "calculates the score modifier" do
+  it "calculates the rating modifier" do
     expect(SimpleElo.modifier(1, 0.5, 10)).to eq(5.0)
   end
 end
